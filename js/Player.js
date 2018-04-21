e4817c8d779bcdf5cb3fedb14e7427c7e086b4f8
SweaterWeatherGames <resonance.data@gmail.com>
2018-04-21 19:16:31 -0400

    Added some more JS files
    These are all things we will need for sure.

--- js/Player.js
+++ js/Player.js
@@ -100,34 +100,3 @@ const Player =function(parent){
         if(this.player.y < 400)this.player.y = 400;
     }
 }
-
-const Gun =function(parent){
-    this.parent = parent;
-    this.isChild = true;
-    this.init = (x, y) =>{
-        this.gun = this.fetchParent().physics.add.image(x,y,'blank');
-        
-        
-        this.gun.update = (dt, right) =>{
-            this.fetchParent().physics.moveToObject(this.gun, this.fetchParent().player, 250);
-            if(right) {
-                this.gun.x = this.fetchParent().player.x + 75;
-                if(this.fetchParent().input.activePointer.buttons == 2 && this.fetchParent().input.activePointer.isDown){
-                    console.log("right click");
-                }
-            }
-            else {
-                this.gun.x = this.fetchParent().player.x - 75;
-                if(this.fetchParent().input.activePointer.buttons == 1 && this.fetchParent().input.activePointer.isDown){
-                    console.log("left click");
-                }
-            }
-        }
-        
-        return this.gun;
-    }
-    this.fetchParent = () =>{
-        if(!this.parent.isChild) return this.parent;
-        else return this.parent.fetchParent();
-    }
-}
